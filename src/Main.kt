ae1cd28675d1ba045c1de0d16be10002e7d38623
Jemimah-Nthenya <jemimahogonda@gmail.com>
2024-03-25 08:36:19 +0300

    add files

---
+++ src/Main.kt
@@ -0,0 +1,24 @@
+fun main() {
+    arr.sum
+    arr.count()
+    arr.average()
+    println(listOf("Barnie","bakes","bage","buns"))
+
+
+
+}
+//question2
+fun arr(array: Array<Int>(num1:Int,num2:Int,num3:Int,num4:Int):Int{
+    var a=10
+    var b=5
+    var c=20
+    var d=4
+    return arr.count()
+}
+
+//question1
+fun arrr(arrayOf(words:String){
+    var words=("Barnie bakes bagel and buns")
+    var list=["Barnie","bakes","bage","buns"]
+    println(list)
+}
